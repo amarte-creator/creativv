6998ebec307ed35ba822a737e919691124bbe844
adr24 <adrian.avilamolina@gmail.com>
2024-11-20 19:22:30 -0300

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { LandingPageComponent } from "@/components/landing-page";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
